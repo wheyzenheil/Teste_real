9f5d73872697665ba227534958dfb89baf451493
wheyzenheil <81117244+wheyzenheil@users.noreply.github.com>
2021-03-21 19:38:44 -0300

    Create Teste_real_03.py

---
+++ Teste_real_03.py
@@ -0,0 +1,6 @@
+nome = input('Digite o nome do cliente: ')
+valor= input('Digite o valor da fatura:' )
+dia = input('Digite o dia de vencimento: ')
+mes = input('Digite o mês de vencimento: ')
+print('Olá', nome)
+print('A sua fatura com vencimento em ',dia,' de ',mes,' no valor de R$ ',valor,' está fechada.')
