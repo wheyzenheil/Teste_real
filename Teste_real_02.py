24bfe4ea67949bba6418650e0c1e1a5eb7972533
wheyzenheil <81117244+wheyzenheil@users.noreply.github.com>
2021-03-21 19:33:25 -0300

    Create Teste_real_02.py

---
+++ Teste_real_02.py
@@ -0,0 +1,7 @@
+print('bem Vindo, este script fara o calculo de meria aritmetica de 4 notas')
+nota1 = float(input('insira o valor da nota 1: '))
+nota2 = float(input('insira o valor da nota 2: '))
+nota3 = float(input('insira o valor da nota 3: '))
+nota4 = float(input('insira o valor da nota 4: '))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print('media aritmetica e de: ', media)
